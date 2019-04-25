5d4862f80d406161fefe2b57d5a8b6d1874b09f1
jenn jiang <yanjunjiang@cca.edu>
2019-04-24 19:01:48 -0700

    pushing project

---
+++ variables.js
@@ -0,0 +1,6 @@
+var bits = 8;
+var fourNumber = 4;
+var fourString = 4;
+console.log(' There are ' + bits + ' bits in a byte. ');
+console.log(fourNumber + ' is a number. ');
+console.log(" ' " + fourString + "' in quotes is a string. ");
