f506076496a86bf832a722ce5f288c993c51cdb6
jenn jiang <yanjunjiang@cca.edu>
2019-04-27 17:47:43 -0700

    pushing project

---
+++ more_variables.js
@@ -0,0 +1,9 @@
+var name = "samson";
+var age = 2;
+var newAge = age + 1;
+var location = "San Francisco";
+
+console.log(' Hey, my name is ' + name + ".");
+console.log(" I'm " + age + ' years old. ');
+console.log(" I'm currently live in " + location + '.');
+console.log(` After my next birthday, I'm going to be ${ newAge } years old. `);
