a9aef262ee1e635c2e123d40de3ddb36d1030682
jenn jiang <yanjunjiang@cca.edu>
2019-05-09 21:31:23 -0700

    pushing project

---
+++ superhero.js
@@ -0,0 +1,8 @@
+var superhero = "ironman";
+var sentence = ` My favorite superhero is ${ superhero }. When he's not ${ superhero }, his name is Peter Parker in the movies.`;
+var sentenceTwo = ` He is part of the Avengers group. ${ superhero } also has his own movies as well. `;
+console.log(sentence + sentenceTwo);
+
+
+
+
