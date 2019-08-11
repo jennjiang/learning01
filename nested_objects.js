1a39ebfa8b7bbfdf073e2155b9c168f0a50040ed
jenn jiang <yanjunjiang@cca.edu>
2019-08-11 16:24:38 -0700

    pushing project

---
+++ nested_objects.js
@@ -0,0 +1,14 @@
+var tvShow = {
+   Drama:"Despaired House Wife",
+   Horror: {
+       name1: "Hunting",
+       name2: "Shadow",
+   },
+   Comedy:{
+       name1: "The Office",
+       name2: "Modern Family",
+   }, 
+};
+
+console.log(tvShow.Comedy.name1);
+console.log(tvShow.Horror.name2);
