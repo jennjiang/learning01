1218bc5c2ea17fded7d1d0c3b7b5b83456d68e04
jenn jiang <yanjunjiang@cca.edu>
2019-08-18 19:49:59 -0700

    pushing project

---
+++ more_array.js
@@ -0,0 +1,6 @@
+var film = ['Kill your darling','The Painted Veil', 'Bicentennial Man'];
+film.push('James and the giant peach','Spirited Away');
+var first = film[0];
+var fifth = film[4];
+console.log(first);
+console.log(fifth);
