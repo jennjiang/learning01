38b4a7ebeb4c059cc7c79032a916a54a047e2cd0
jenn jiang <yanjunjiang@cca.edu>
2019-09-15 22:49:10 -0700

    new project

---
+++ quiz/quiz_02.js
@@ -0,0 +1,13 @@
+function farenheitToCelsius(faren) {
+  return (((faren - 32) * 5) / 9).toFixed(5);
+}
+
+function printCurrent(temp, degree) {
+  var deg = degree || "C";
+  console.log(`${temp} º${deg}`);
+}
+
+printCurrent(farenheitToCelsius(61), "C");
+// should print out 16.1 ºC
+printCurrent(farenheitToCelsius(104));
+// should print out 40.0 ºC
