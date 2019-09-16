733c359b9bd7d380fce1783ab565eedc1c2d2f5c
jenn jiang <yanjunjiang@cca.edu>
2019-09-15 22:20:37 -0700

    pushing project

--- morenested_object.js
+++ morenested_object.js
@@ -1,19 +1,13 @@
 var heavyData = {
-    Boolean:{
-        bool1:'true',
-        bool2:'false',
-    },
-
-    string:{
-        number:'723',
-        words:"ABC",
-    },
-
-    integer :'6', 
-    float:'51.43.2', 
-    list:['tea', 'milk', 'sugar']
-
+  string: "723",
+  integer: 6,
+  float: 51.43,
+  list: ["tea", "milk", "sugar"]
 };
 
-console.log(heavyData.list[2]);
-console.log(heavyData.Boolean.bool1);
+var number = {
+  isOdd: false
+};
+
+console.log(heavyData.list);
+console.log(number);
